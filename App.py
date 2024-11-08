90e46e293f7d8634d36dea6e92fe7eb602f541c4
Shubham Mahadwale <shubham_mahadwale@whirlpool.com>
2024-11-08 13:09:55 +0530

    stOVFL

--- App.py
+++ App.py
@@ -121,9 +121,9 @@ async def doBleFtp():
             pass
         cIndex += 1
         if cIndex % 20 == 0:
-            delay_tm = 2
+            delay_tm = 0.1
         elif cIndex % 100 == 0:
-            delay_tm = 4
+            delay_tm = 0.5
         else:
             delay_tm = 0.01
         await BLEclient.write_gatt_char(fileDataChar, i, response=True)
