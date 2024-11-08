360c90417708c402940a6ab7a131c033c4bdc4c5
Shubham Mahadwale <shubham_mahadwale@whirlpool.com>
2024-11-08 12:29:40 +0530

    nne

--- App.py
+++ App.py
@@ -115,7 +115,7 @@ async def doBleFtp():
         retryIndex = 0
         statusVal = 0
         #print(i)
-        while(time.time() - prevTime > 0.025):
+        while(time.time() - prevTime < 0.025):
             pass
         await BLEclient.write_gatt_char(fileDataChar, i, response=True)
         prevTime = time.time()
